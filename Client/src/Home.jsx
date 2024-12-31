43a22abc7d2ce8a2b30bc48393c9bdd40d1cbe18
Divya Dukhande <ddukhande06@gmail.com>
2024-12-31 23:41:53 +0530

    Home page updated

--- Client/src/Home.jsx
+++ Client/src/Home.jsx
@@ -3,23 +3,23 @@ import React from "react";
 const Home = () => {
   return (
     <main className="w-full flex flex-col items-center justify-center p-8 mb-20 mt-24">
-      <section className="w-10/12 max-w-5xl py-8 bg-gradient-to-r from-yellow-300 via-yellow-400 to-yellow-300 p-6 rounded-lg shadow-lg">
+      <section className="w-10/12 max-w-6xl py-8 bg-gradient-to-r from-yellow-300 via-yellow-400 to-yellow-300 p-6 rounded-lg shadow-lg">
         <div className="flex flex-col md:flex-row items-center justify-between gap-4 mx-auto">
           {/* Left Side: Image */}
           <div className="flex-1 flex justify-center items-center">
             <img
-              src="/assets/images/Natraj.jpg"
+              src="/assets/images/Natraj.png"
               alt="Dance Pose"
-              className="h-56 w-56 object-contain rounded-lg shadow-md"
+              className="h-56 w-58 object-contain rounded-lg shadow-md"
             />
           </div>
 
           {/* Right Side: Text */}
-          <div className="flex-1 text-center p-10 md:text-right">
-            <p className="text-lg text-red-700">
+          <div className="flex-1 text-center p-12 ">
+            <p className="text-lg text-2xl font-semibold  text-red-700">
               Angikam bhuvanam yasya vacikam sarvavanmayam।
             </p>
-            <p className="text-lg mt-2 text-red-700">
+            <p className="text-lg text-2xl font-semibold mt-2  text-red-700">
               Aharyam chandrataradi tam numah sattvikam sivam।।
             </p>
           </div>
