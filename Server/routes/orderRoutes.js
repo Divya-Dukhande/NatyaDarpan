a9f05b0a8b2c609f5267b29de91c98da2c3f8e84
Divya Dukhande <ddukhande06@gmail.com>
2025-03-29 18:47:49 +0530

    filters are added

--- Server/routes/orderRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getUserOrders } = require('../controllers/orderController');
-
-const router = express.Router();
-
-// Get orders for a specific user
-router.get('/user-orders/:userId', getUserOrders);
-
-module.exports = router;
